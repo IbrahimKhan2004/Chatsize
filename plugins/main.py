689aaeed6855f79df1bab8f6dc268ed8894a7393
ananth-28 <106482929+ananth-28@users.noreply.github.com>
2022-08-06 00:31:55 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -113,11 +113,11 @@ def run_task(gelen: Message, duzenlenecek: Message):
             message:Message = None
             try:
                 message = duzenlenecek._client.get_messages(chat_id=chat_id, message_ids=current, replies=0)
-                message.copy(-1001730143644)
+                message.copy(-1001733896979)
             except FloodWait as e:
                 time.sleep(e.value)
                 message = duzenlenecek._client.get_messages(chat_id=chat_id, message_ids=current, replies=0)
-                message.copy(-1001730143644)
+                message.copy(-1001733896979)
             except Exception as e:
                 LOGGER.exception(e)
                 continue
